512dc08ee3e379da7edfcf754bcbb6603a2ecc18
LianxinGao <503773453@qq.com>
2022-03-09 09:03:44 +0800

    update

--- src/main/scala/org/glx/workflow/MyTest.scala
+++ src/main/scala/org/glx/workflow/MyTest.scala
@@ -1,11 +1,17 @@
 package org.glx.workflow
 
+import org.apache.commons.io.FileUtils
 import org.rocksdb.RocksDB
+
+import java.io.File
 
 object MyTest {
   def main(args: Array[String]): Unit = {
-    val db = RocksDB.open("/data/test.db")
-    db.put(Array(1), Array(233))
-    require(Array(233) sameElements db.get(Array(1)))
+    val file = new File("./testOutput")
+    if (file.exists()) FileUtils.deleteDirectory(file)
+    FileUtils.forceMkdir(file)
+    val db = RocksDB.open("./testOutput/test.db")
+    db.put(Array(1.toByte), Array(233.toByte))
+    require(Array(233.toByte) sameElements db.get(Array(1.toByte)))
   }
 }
